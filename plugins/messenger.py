3b74a88ad9250dbc597f169bbb412ee2127ac493
Jakub Stasiak <jakub@stasiak.at>
2013-02-03 14:49:04 +0000

    Introduce messenger plugin

--- plugins/messenger.py
+++ plugins/messenger.py
@@ -1,8 +1,27 @@
+from injector import inject
+from flask import Request
+
 from hal.response import Envelope
+from hal.user import User
 
 def plugin(bot):
-	@bot.web.route('/echo')
-	def echo():
-		envelope = Envelope(user = None, room = 'staff')
-		bot.send(envelope, 'echo!')
+	@bot.web.route('/talk', methods = ['POST'])
+	@inject(request = Request)
+	def talk(request):
+		room = request.form['room']
+		name = request.form.get('user') or None
+		if name:
+			user = User(id = name, name = name, room = room)
+		else:
+			user = None
+
+		message = request.form['message']
+		envelope = Envelope(user = user, room = room)
+
+		if user:
+			method = bot.reply
+		else:
+			method = bot.send
+
+		method(envelope, message)
 		return 'ok'
