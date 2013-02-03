d30d8f53e0925ce2e74634c538348dc144069e52
Jakub Stasiak <jakub@stasiak.at>
2013-02-03 21:28:01 +0000

    Create calculator plugin

---
+++ plugins/calc.py
@@ -0,0 +1,20 @@
+__commands__ = '''
+	hal (calc|calculate) [me] <expression> - calculates the expression
+	hal (calc|calculate|convert) [me] <expression> (to|in) <units> - converts expression to specified units
+'''
+
+import execjs
+import json
+
+import requests
+
+def plugin(bot):
+	@bot.respond('(calc|calculate|convert)( me)? (.+)')
+	def calc(response):
+		query = response.match.group(3)
+
+		params = dict(hl = 'en', q = query)
+		response = requests.get('https://www.google.com/ig/calculator', params = params)
+		mapping = execjs.eval(response.content)
+		rhs, error = mapping['rhs'], mapping['error']
+		return rhs or error
