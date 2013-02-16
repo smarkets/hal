457bf12a0a7a01628390855510f4d3187380ecac
Jakub Stasiak <jakub@stasiak.at>
2013-02-16 15:54:25 +0000

    Create locally stored wat image list

--- plugins/wat.py
+++ plugins/wat.py
@@ -2,10 +2,24 @@ __commands__ = '''
 	wat - provides link to random WAT image
 '''
 
+import random
 import requests
+
+local_wats = (
+	'http://s.telegraph.co.uk/graphics/html/Years/2013/images/owl4.gif',
+)
 
 def plugin(bot):
 	@bot.hear('wat$')
 	def wat(response):
-		r = requests.get('http://watme.herokuapp.com/random')
-		return r.json()['wat']
+
+		# my approximation
+		remote_wats_number = 50
+
+		if random.randint(1, remote_wats_number + len(local_wats)) <= remote_wats_number:
+			r = requests.get('http://watme.herokuapp.com/random')
+			wat_link = r.json()['wat']
+		else:
+			wat_link = random.choice(local_wats)
+
+		return wat_link
