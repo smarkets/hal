486489829ea63f4de4576439f83437267ba79136
Jakub Stasiak <jakub@stasiak.at>
2014-08-11 15:11:52 +0100

    Move main function to hal.main

---
+++ hal/main.py
@@ -0,0 +1,33 @@
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+import logging
+
+from argparse import ArgumentParser
+from imp import load_source
+from os.path import join
+
+from injector import AssistedBuilder, Injector
+
+from hal import PROJECT_ROOT
+from hal.bot import Bot
+from hal.modules import ApplicationModule
+
+
+def main():
+    root_logger = logging.getLogger()
+    root_logger.addHandler(logging.StreamHandler())
+    root_logger.level = logging.DEBUG
+
+    parser = ArgumentParser(description='HAL bot')
+    parser.add_argument('--adapter', dest='adapter', default='shell')
+    parser.add_argument('--name', dest='name', default='HAL')
+    arguments = parser.parse_args()
+
+    injector = Injector([ApplicationModule])
+    bot_builder = injector.get(AssistedBuilder(Bot))
+    bot = bot_builder.build(name=arguments.name)
+
+    adapter_module = load_source('adapter', join(PROJECT_ROOT, 'adapters',
+                                                 '%s.py' % (arguments.adapter,)))
+    bot.adapter = adapter_module.Adapter(bot)
+    bot.run()
--- main.py
+++ main.py
@@ -1,33 +1,6 @@
 #!/usr/bin/env python
-from __future__ import absolute_import, unicode_literals
 
-import logging
-
-from argparse import ArgumentParser
-from imp import load_source
-from os.path import join
-
-from injector import AssistedBuilder, Injector
-
-from hal import PROJECT_ROOT
-from hal.bot import Bot
-from hal.modules import ApplicationModule
+from hal.main import main
 
 if __name__ == '__main__':
-    root_logger = logging.getLogger()
-    root_logger.addHandler(logging.StreamHandler())
-    root_logger.level = logging.DEBUG
-
-    parser = ArgumentParser(description='HAL bot')
-    parser.add_argument('--adapter', dest='adapter', default='shell')
-    parser.add_argument('--name', dest='name', default='HAL')
-    arguments = parser.parse_args()
-
-    injector = Injector([ApplicationModule])
-    bot_builder = injector.get(AssistedBuilder(Bot))
-    bot = bot_builder.build(name=arguments.name)
-
-    adapter_module = load_source('adapter', join(PROJECT_ROOT, 'adapters',
-                                                 '%s.py' % (arguments.adapter,)))
-    bot.adapter = adapter_module.Adapter(bot)
-    bot.run()
+    main()
